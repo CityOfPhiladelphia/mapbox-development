9a9342c3bc40112048d59912375ffd5dbd4e16a5
andy.rothwell <rothwell.andy@gmail.com>
2020-02-13 09:59:25 -0500

    testing travis again

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import 'phila-standards/dist/css/phila-app.min.css';
 // import 'mapbox-gl/dist/mapbox-gl.css'
 import 'leaflet/dist/leaflet.css';
 
-
 let config = {
   map: {
     containerClass: null,
